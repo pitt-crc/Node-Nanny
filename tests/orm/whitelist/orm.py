3d60f06924f24e1f3d2cd7bfdc7b5c5b137dc3e8
Daniel <djperrefort@gmail.com>
2022-06-10 16:45:16 -0400

    Moves ORM tests into dedicated directory

--- tests/orm/whitelist/orm.py
+++ tests/orm/whitelist/orm.py
@@ -1,9 +1,12 @@
+"""Tests for the ORM ``Whitelist`` table."""
+
 from unittest import TestCase
 
 from node_nanny.orm import Whitelist
 
 
-class WhitelistExpirationNullable(TestCase):
+class NullableColumns(TestCase):
+    """Test columns that need to be nullable are nullable"""
 
     def test_expiration_nullable(self):
         """Values for the ``termination`` column should be nullable"""
