7671ef8b733c98ca4b4e1387d7698c48e733d39e
Daniel <djperrefort@gmail.com>
2022-06-13 11:07:12 -0400

    Fills in logic for UsageMonitor class

--- node_nanny/utils.py
+++ node_nanny/utils.py
@@ -1,5 +1,6 @@
 """Utilities for fetching and interacting with system data."""
 
+import psutil
 from pandas import DataFrame
 
 
@@ -7,14 +8,47 @@ class UsageMonitor:
     """Fetch current system usage information"""
 
     @staticmethod
-    def current_usage() -> DataFrame:
+    def _get_process_data(pid: int) -> dict:
+        """Return the current system usage for a single running processes
+
+        Args:
+            pid: The ID of a currently running process
+
+        Returns:
+            A dictionary with system usage information for the given proces ID
+
+        Raises:
+            psutil.NoSuchProcess: If the given process ID can not be found
+        """
+
+        pr = psutil.Process(pid)
+        return {
+            'PID': pid,
+            'PNAME': pr.name(),
+            'USER': pr.username(),
+            'STATUS': pr.status(),
+            'CPU': f'{pr.cpu_percent():.2f}%',
+            'MEM': f'{pr.memory_percent():.2f}%'
+        }
+
+    @classmethod
+    def current_usage(cls) -> DataFrame:
         """Return the current system usage for all running processes
 
         Returns:
             A ``DataFrame`` of currently running processes
         """
 
-        raise NotImplementedError
+        data = []
+        for process in psutil.pids():
+            try:
+                data.append(cls._get_process_data(process))
+
+            # Some subprocesses may exit while fetching process info
+            except psutil.NoSuchProcess:
+                pass
+
+        return DataFrame(data).set_index(['USER', 'PID'])
 
     def user_usage(self, username: str) -> DataFrame:
         """Return the current system usage for all processes tied to a given user
@@ -26,4 +60,8 @@ class UsageMonitor:
             A ``DataFrame`` of running processes tied to the given user
         """
 
-        raise NotImplementedError
+        try:
+            return self.current_usage().loc[username]
+
+        except KeyError:
+            raise ValueError(f'No running processes found for user {username}')
