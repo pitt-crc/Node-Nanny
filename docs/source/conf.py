a9d585224b5e6e1713966bb03af7936ed95cbb44
Daniel <djperrefort@gmail.com>
2022-06-22 09:55:02 -0400

    Adds sphinx plugins

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,19 +1,13 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+# See the documentation: https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 # -- Path setup --------------------------------------------------------------
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import sys
+from pathlib import Path
 
+package_source_path = Path(__file__).resolve().parent.parent.parent
+sys.path.insert(0, str(package_source_path))
 
 # -- Project information -----------------------------------------------------
 
@@ -24,29 +18,23 @@ author = 'Pitt Center for Research Computing'
 
 # -- General configuration ---------------------------------------------------
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
+# Add any Sphinx extension module names here, as strings.
+# Note: ``sphinx.ext.napoleon`` need to be loaded BEFORE ``sphinx_autodoc_typehints``
 extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.doctest',
+    'sphinx.ext.napoleon',
+    'sphinx_copybutton',
+    'sphinx_autodoc_typehints',
+    'sphinx_rtd_theme',
+    'sphinxarg.ext',
+    'sphinx.ext.githubpages'
 ]
-
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
-
 
 # -- Options for HTML output -------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = 'alabaster'
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+html_css_files = ['css/custom.css']
