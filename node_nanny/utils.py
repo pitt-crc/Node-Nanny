2c80b725435032117db40ee98e76a5834b971c82
Daniel Perrefort <djperrefort@gmail.com>
2022-07-05 09:06:43 -0400

    First pass at implimenting UserNotifier class (#21)
    * Adds foriegn keys to database tables
    
    * First pass outline for UserNotifier class
    
    * Adds DataAccessLayer class for managing database connections
    
    * Adds test for empty database queries
    
    * Outlines logic for sending email notifications
    
    * Adds test for fetching notification history from DB
    
    * Adds pandas to requirements file
    
    * Renames DataAccessLayer to DBConnection
    
    * Uses full mock domain name in notification history test

--- node_nanny/utils.py
+++ node_nanny/utils.py
@@ -1,7 +1,83 @@
 """Utilities for fetching and interacting with system data."""
 
+from datetime import datetime
+from email.message import EmailMessage
+from smtplib import SMTP
+
 import psutil
-from pandas import DataFrame
+from pandas import DataFrame, read_sql
+from sqlalchemy import select
+
+from node_nanny.orm import Notification, User, db
+
+
+class UserNotifier:
+    """Handles the sending and tracking of user email notifications"""
+
+    def __init__(self, username: str) -> None:
+        """Manage email notifications for the given user
+
+        Args:
+            username: The name of the user to manage notifications for
+        """
+
+        self._username = username
+
+    def get_user_email(self) -> str:
+        """Return the email corresponding to the current user"""
+
+        return self._username + '@pitt.edu'
+
+    def notification_history(self) -> DataFrame:
+        """Return a tabular summary of previous user notifications
+
+        Returns:
+            DataFrame with user notification history
+        """
+
+        query = select(
+            Notification.node, Notification.time, Notification.memory, Notification.percentage
+        ).join(User).where(User.name == self._username)
+
+        return read_sql(query, db.engine)
+
+    def notify(self, node: str, usage: DataFrame, limit: int) -> None:
+        """Notify the user their running processes have been killed
+
+        Args:
+            node: Hostname of node their processes were running on
+            usage: System information for the killed processes
+            limit: The memory limit used to trigger the notification
+        """
+
+        # Update the notification table in the database
+        with db.session() as session:
+            # If the user has not been notified before, create a new User record
+            user_query = select(User).where(User.name == self._username)
+            user = session.execute(user_query).scalars().first()
+            if user is None:
+                user = User(name=self._username)
+
+            notification = Notification(
+                user=user,
+                node=node,
+                time=datetime.now(),
+                percentage=usage.MEM.sum(),
+                limit=limit
+            )
+
+            session.add(user)
+            session.add(notification)
+            session.commit()
+
+        message = EmailMessage()
+        message.set_content("This is email text")
+        message["Subject"] = "Email subject"
+        message["From"] = "from_user@dummy.domain.edu"
+        message["To"] = self.get_user_email()
+
+        with SMTP("localhost") as smtp:
+            smtp.send_message(message)
 
 
 class SystemUsage:
