95a84e5260ce9fd7f1033cebbf96cb4e60306df9
Nickolas Comeau <nickolas.comeau@pitt.edu>
2022-07-12 14:44:47 -0400

    remove some extraneous parameters for a few argument definitions, fix command pop from args

--- node_nanny/cli.py
+++ node_nanny/cli.py
@@ -1,19 +1,19 @@
 """Command line argument parser for node usage monitor"""
 
-from argparse import ArgumentParser
-import sys
+from argparse import ArgumentParser, ArgumentError
 from socket import gethostname
 from app import MonitorUtility
 
 class CLIParser(ArgumentParser):
+    """Class for passing command line arguments to the MonitorUtility app"""
 
     def __init__(self):
         """Define arguments for the command line interface"""
         super(CLIParser, self).__init__()
-        self.add_argument('-v', '--version', action='version', version=self.app_version)
 
         # Command subparser
-        command = self.add_subparsers(dest='command',help='subcommand to run. Options are scan, kill, whitelist, add, remove')
+        command = self.add_subparsers(dest='command', parser_class=ArgumentParser, help='subcommand to run. Options are scan, kill, whitelist, add, remove')
+        command.required = True
 
         ## Scan for Processes
         scan = command.add_parser('scan', help='Initiate a scan for memory intensive processes')
@@ -24,7 +24,7 @@ class CLIParser(ArgumentParser):
         ## Kill User's Processes
         kill = command.add_parser('kill',help='Kill all running processes attached to the fiven username and notify the them via email')
         kill.add_argument('-u', '--user', action='store', type=str, required=True, help='Username of the user whos jobs should be killed')
-        kill.add_argument('-q', '--quiet', action='store', type=boolean, required=False, help='suppress outgoing email notifications') 
+        kill.add_argument('-q', '--quiet', action='store', type=bool, required=False, help='suppress outgoing email notifications') 
 
         ## Show Whitelist
         whitelist = command.add_parser('whitelist',help='display current whitelist, user and node names')
@@ -39,14 +39,14 @@ class CLIParser(ArgumentParser):
         add.add_argument('-d', '--duration', action='store', type=int, required=False, default=100000000, help='How long to whitelist a user for')
         node_opts = add.add_mutually_exclusive_group()
         node_opts.add_argument('-n', '--node', action='store', type=str, required=False,default=gethostname(), help='Node or list of nodes to whitelist the user on')
-        node_opts.add_argument('-g', '--global', action='store_true', const=True, type=bool, default=False, help='Whitelist user on all nodes')
+        node_opts.add_argument('-g', '--global', action='store_true', help='Whitelist user on all nodes')
 
         ## Remove from whitelist
         remove = command.add_parser('remove', help='Remove a user from the whitelist on the current node')
         remove.add_argument('-u', '--user', action='store', type=str, required=True, help='Username of the user who should be removed from the whitelist')
         node_opts = remove.add_mutually_exclusive_group()
         node_opts.add_argument('-n', '--node', action='store', type=str, required=False,default=gethostname(), help='Node or list of nodes to Un-whitelist the user on')
-        node_opts.add_argument('-g', '--global', action='store_true', const=True, type=bool, default=False, help='Un-Whitelist user on all nodes')
+        node_opts.add_argument('-g', '--global', action='store_true', help='Un-Whitelist user on all nodes')
 
 
     def error(self, message):
@@ -61,14 +61,13 @@ class CLIParser(ArgumentParser):
     def execute(self):
         """Parse command line arguments and execute usage monitor"""
 
-        try:
+        try: 
             args = self.parse_args()
-            function = args.pop('command', self.print_help)
-
+            function = args.__dict__.pop('command')
             app = MonitorUtility()
             app.function(**args)
 
-        except argparse.ArgumentError:
+        except ArgumentError:
             exit('Something is wrong with the arguments you provided. exiting...')
         except KeyboardInterrupt:
             exit('Keyboard interrupt detected! exiting...')
