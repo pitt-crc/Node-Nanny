b3b22fa971beebc6d530601048d47adb53232213
Daniel <djperrefort@gmail.com>
2022-07-12 10:13:27 -0400

    Create index page dynamically

---
+++ node_nanny/__init__.py
@@ -0,0 +1,25 @@
+"""
+CRC Usage Monitor
+=================
+
+This is documentation.
+
+.. toctree::
+   :hidden:
+
+   Overview<self>
+
+.. Source files for documenting individual applications are generated
+   dynamically by the sphinx-autoapi plugin. We add specific files to ensure
+   their order in the sidebar. Any remaining files are added via glob.
+
+.. toctree::
+   :hidden:
+   :caption: Applications:
+   :glob:
+
+   app/index
+   utils/index
+   orm/index
+   **/[!_]*
+"""
