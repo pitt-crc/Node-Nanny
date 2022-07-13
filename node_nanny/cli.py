59fcf504d9fe8e287cdac8469f2d12bb7610cbd1
Nickolas Comeau <nickolas.comeau@pitt.edu>
2022-07-13 11:48:51 -0400

    Fix how app functionality is called in the execute method

--- node_nanny/cli.py
+++ node_nanny/cli.py
@@ -3,7 +3,7 @@
 import sys
 from argparse import ArgumentParser, ArgumentError
 from socket import gethostname
-from app import MonitorUtility
+from node_nanny.app import MonitorUtility
 
 class CLIParser(ArgumentParser):
     """Class for passing command line arguments to the MonitorUtility app"""
@@ -18,6 +18,7 @@ class CLIParser(ArgumentParser):
 
         ## Scan for Processes
         scan = command.add_parser('scan', help='Initiate a scan for memory intensive processes')
+
         scan.add_argument('-M', '--max-mem', action='store', type=int, required=False, default=20, help='Max limit on memory usage, any process above this will be killed.')
         scan.add_argument('-m', '--min-mem', action='store', type=int, required=False, default=5, help='Lower limit on memory usage, processes under this amount will never be killed.')
         scan.add_argument('-w', '--wait', action='store', type=int, required=False, default=5, help='Duration that a process found to be above the memory limit is allowed to continue before being killed.')
@@ -60,7 +61,7 @@ class CLIParser(ArgumentParser):
         # Print help when no arguements are provided
         if len(sys.argv) == 1:
             self.print_help()
-            return
+            exit()
 
         print("An error occurred: " + message)
         return
@@ -68,13 +69,10 @@ class CLIParser(ArgumentParser):
     def execute(self):
         """Parse command line arguments and execute usage monitor"""
 
-        try: 
-            args = self.parse_args()
-            function = args.__dict__.pop('command')
+        try:
+            args = self.parse_args().__dict__
             app = MonitorUtility()
-            app.function(**args)
+            getattr(app,args.pop('command'))(**args)
 
-        except ArgumentError:
-            exit('Something is wrong with the arguments you provided. exiting...')
         except KeyboardInterrupt:
             exit('Keyboard interrupt detected! exiting...')
