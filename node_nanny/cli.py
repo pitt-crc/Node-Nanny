30a79f39da64a430cb552386dfd5ee14fa7a469d
Nickolas Comeau <nickolas.comeau@pitt.edu>
2022-07-14 11:33:07 -0400

    PEP 8 / codacy formatting, remove try/except in execute

--- node_nanny/cli.py
+++ node_nanny/cli.py
@@ -5,25 +5,27 @@ from argparse import ArgumentParser
 from socket import gethostname
 from node_nanny.app import MonitorUtility
 
+
 class CLIParser(ArgumentParser):
     """Class for passing command line arguments to the MonitorUtility app"""
 
     def __init__(self):
         """Define arguments for the command line interface"""
-        super(CLIParser, self).__init__()
+
+        super().__init__()
 
         # Command subparser
         command = self.add_subparsers(
             dest='command',
-            parser_class=ArgumentParser,
+            parser_class=self.__class__,
             required=True,
-            help='Sub-command to run'
+            help='sub-command to run'
         )
 
         ## Scan subcommand
         scan = command.add_parser(
             'scan',
-            help='Initiate a scan for memory intensive processes'
+            help='initiate a scan for memory intensive processes'
         )
 
         scan.add_argument(
@@ -33,7 +35,7 @@ class CLIParser(ArgumentParser):
             type=int,
             required=False,
             default=20,
-            help=('Max limit on memory usage, '
+            help=('max limit on memory usage, '
                 'any process above this will be killed')
         )
 
@@ -44,7 +46,7 @@ class CLIParser(ArgumentParser):
             type=int,
             required=False,
             default=5,
-            help=('Lower limit on memory usage, '
+            help=('lower limit on memory usage, '
             'processes under this amount will never be killed')
         )
 
@@ -55,7 +57,7 @@ class CLIParser(ArgumentParser):
             type=int,
             required=False,
             default=5,
-            help=('Duration that a process found to be above '
+            help=('duration that a process found to be above '
             'the memory limit is allowed to continue before being killed')
         )
 
@@ -72,7 +74,7 @@ class CLIParser(ArgumentParser):
             action='store',
             type=str,
             required=True,
-            help='Username of the user whos jobs should be killed'
+            help='username of the user whos jobs should be killed'
         )
 
         kill.add_argument(
@@ -88,7 +90,7 @@ class CLIParser(ArgumentParser):
         ## Notification History subcommand
         history = command.add_parser(
             'history',
-            help = ('Display a history of recently killed jobs '
+            help = ('display a history of recently killed jobs '
             'and corresponding email notifications')
         )
 
@@ -98,18 +100,18 @@ class CLIParser(ArgumentParser):
             action='store',
             type=str,
             required=False,
-            help='Username of the notification history to show'
+            help='username of the notification history to show'
         )
 
         ## Whitelist subcommand
-        whitelist = command.add_parser(
+        command.add_parser(
         'whitelist',
         help='display current whitelist, user and node names'
         )
 
         add = command.add_parser(
             'add',
-            help=('Whitelist the given user so their jobs are '
+            help=('whitelist the given user so their jobs are '
             'never killed on the current node')
         )
 
@@ -119,7 +121,7 @@ class CLIParser(ArgumentParser):
             action='store',
             type=str,
             required=True,
-            help='Username of the user who should be added to the whitelist'
+            help='username of the user who should be added to the whitelist'
         )
 
         add.add_argument(
@@ -129,7 +131,7 @@ class CLIParser(ArgumentParser):
             type=int,
             required=False,
             default=100000000,
-            help='How long to whitelist a user for'
+            help='how long to whitelist a user for'
         )
 
         whitelist_node_opts = add.add_mutually_exclusive_group()
@@ -141,20 +143,20 @@ class CLIParser(ArgumentParser):
             type=str,
             required=False,
             default=gethostname(),
-            help='Node or list of nodes to whitelist the user on'
+            help='node or list of nodes to whitelist the user on'
         )
 
         whitelist_node_opts.add_argument(
             '-g',
             '--global',
             action='store_true',
-            help='Whitelist user on all nodes'
+            help='whitelist user on all nodes'
         )
 
         ## Remove from whitelist
         remove = command.add_parser(
             'remove',
-            help='Remove a user from the whitelist on the current node'
+            help='remove a user from the whitelist on the current node'
         )
 
         remove.add_argument(
@@ -163,7 +165,7 @@ class CLIParser(ArgumentParser):
             action='store',
             type=str,
             required=True,
-            help=('Username of the user who should be removed from '
+            help=('username of the user who should be removed from '
             'the whitelist')
         )
 
@@ -175,14 +177,14 @@ class CLIParser(ArgumentParser):
             type=str,
             required=False,
             default=gethostname(),
-            help='Node or list of nodes to Un-whitelist the user on'
+            help='node or list of nodes to Un-whitelist the user on'
         )
 
         node_opts.add_argument(
             '-g',
             '--global',
             action='store_true',
-            help='Un-Whitelist user on all nodes'
+            help='un-whitelist user on all nodes'
         )
 
 
@@ -193,21 +195,16 @@ class CLIParser(ArgumentParser):
             message: The error message
         """
 
-        # Print help when no arguements are provided
+        # Print help when no arguments are provided
         if len(sys.argv) == 1:
             self.print_help()
             sys.exit()
 
         print("An error occurred: " + message)
-        return
 
     def execute(self):
         """Parse command line arguments and execute usage monitor"""
 
-        try:
-            args = vars(self.parse_args())
-            app = MonitorUtility()
-            getattr(app,args.pop('command'))(**args)
-
-        except KeyboardInterrupt:
-            exit('Keyboard interrupt detected! exiting...')
+        args = vars(self.parse_args())
+        app = MonitorUtility()
+        getattr(app,args.pop('command'))(**args)
